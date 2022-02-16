5b215350818fb17f80129503ea86ddbb9e3041e2
LioQing <46854695+LioQing@users.noreply.github.com>
2022-02-16 22:32:28 +0800

    Remove the sfml and box2d headers in include

--- include/doge/utils/Color.hpp
+++ include/doge/utils/Color.hpp
@@ -2,6 +2,12 @@
 
 #include <cstdint>
 
+#if defined(WIN32) || defined(_WIN32)
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT
+#endif
+
 namespace doge
 {
     struct Color
